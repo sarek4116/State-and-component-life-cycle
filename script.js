2479f5c0a56d58bea514a6ae65e7a74bb67e2d1b
sarek <sarek4116@gmail.com>
2019-07-08 21:20:07 +0200

    add scripts

---
+++ script.js
@@ -0,0 +1,35 @@
+var Counter = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
+
+    render: function() {
+        return React.createElement('div', {},
+            React.createElement('span', {}, 'Licznik ' + this.state.counter),
+            React.createElement('button', {onClick: this.increment}, '+'),
+            React.createElement('button', {onClick: this.decrement}, '-')
+        );
+    }
+});
+
+var element = React.createElement ('div', {},
+ React.createElement(Counter),
+ React.createElement(Counter),
+ React.createElement(Counter)
+)
+
+ReactDOM.render(element, document.getElementById('app'));
