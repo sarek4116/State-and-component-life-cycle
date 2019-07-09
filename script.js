5cc66fb6a8ee93ad011d1845e319f7da101c8289
sarek <sarek4116@gmail.com>
2019-07-09 22:27:01 +0200

    add lifecycle instruction

--- script.js
+++ script.js
@@ -5,6 +5,34 @@ var Counter = React.createClass({
         };
     },
 
+    componentWillMount: function() {
+		console.log('Metoda ta jest wywoływana przed przed wywołaniem metody render')
+      },
+      
+    componentDidMount: function () {
+          console.log('Metoda ta pozwala zakutalizować stan komponentu przy wywołaniu np Ajax')
+    },
+
+    componentWillReceiveProps: function() {
+        console.log('Do metody tej przekazywany jest obiekt zawierający nowe propsy i dzięki tej metodzie możemy sprawdzić czy komponent zostanie odświeżony po otrzymaniu nowych propsów');
+    },
+
+    shouldComponentUpdate: function() {
+        console.log("Dzięki tej metodzie możemy zadecydować czy komponent ma się odświeżyć");
+    },
+
+    componentWillUpdate: function() {
+        console.log("Metoda ta jest podobna do metody componentWillMount, jest ona również wywoływana przed metodą render");
+    },
+
+    componentDidUpdate: function() {
+        console.log('Metoda ta w argumentach zawiera poprzednie stany i propsy, również dzięki niej możemy zakutalizować stan komponentu');          
+    },
+
+    componentWillUnmount: function() {
+        console.log('Metoda ta nie przyjmuje żadnych argumentów i możemy jej użyć do czyszczenia, oraz anulowania żądań')
+    },
+
     increment: function() {
         this.setState({
             counter: this.state.counter + 1
